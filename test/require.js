54009fbe546a88e5d74e648b78b6c5caf01c23d7
Jakob Voss <voss@gbv.de>
2016-08-21 14:40:44 +0200

    Move more parts into module (#6)

--- test/require.js
+++ test/require.js
@@ -7,7 +7,9 @@ describe('wikidata-taxonomy library', () => {
     expect(wdt.serializer.json).to.be.function  
     expect(wdt.serializer.tree).to.be.function  
   })
-  it('should export serialize', () => {
+  it('should export query, taxonomy & serialize', () => {
+    expect(wdt.query).to.be.function  
+    expect(wdt.taxonomy).to.be.function  
     expect(wdt.serialize).to.be.function  
   })
 })
--- wdtaxonomy.js
+++ wdtaxonomy.js
@@ -2,11 +2,8 @@
 
 // dependencies
 var chalk	= require('chalk')
-var wdk 	= require('wikidata-sdk')
 var program = require('commander')
-var request = require('request-promise')
 var Promise = require('bluebird')
-
 var wdt = require('./lib/wikidata-taxonomy.js')
 
 // print error and exit
@@ -15,163 +12,6 @@ function error(code) {
   args[0] = chalk.red(args[0])
   console.error.apply(null, args)
   process.exit(code)
-}
-
-// normalize and check Wikidata identifier
-function normalizeId(id) {
-  if (id === undefined) {
-    error(1,"missing id argument")
-  }
-  try {
-    return wdk.normalizeId(id)
-  } catch (err) {
-    error(1,"invalid id: %s", id)
-  }
-}
-
-// construct basic SPARQL query
-function mainSparqlQuery(root, env) {
-  var language       = env.language
-  var countInstances = !env.noCountInstances && !env.reverse 
-  var property = env.properties ? 'P1647' : 'P279'
-  var reachable = env.reverse
-                ? "wd:"+root+" wdt:"+property+"* ?item"
-                : "?item wdt:"+property+"* wd:"+root
-
-  var sparql = `SELECT ?item ?itemLabel ?broader ?parents ?instances ?sites
-WHERE {
-    {
-        SELECT ?item (count(distinct ?parent) as ?parents) {
-            ${reachable}
-            OPTIONAL { ?item wdt:${property} ?parent }
-        } GROUP BY ?item
-    }
-`
-    if (!env.property) { // TODO: get usage of property
-      if (countInstances) {
-        sparql += `    {
-        SELECT ?item (count(distinct ?element) as ?instances) {
-            ${reachable}
-            OPTIONAL { ?element wdt:P31 ?item }
-        } GROUP BY ?item
-    }
-`     }
-      sparql += `    {
-        SELECT ?item (count(distinct ?site) as ?sites) {
-            ${reachable}
-            OPTIONAL { ?site schema:about ?item }
-        } GROUP BY ?item
-    }
-`
-    }
-
-    sparql += `    OPTIONAL { ?item wdt:${property} ?broader }
-    SERVICE wikibase:label {
-        bd:serviceParam wikibase:language "${language}" .
-    }
-}`
-	return sparql
-}
-
-// SPARQL query to get all instances
-function instancesSparqlQuery(root, language) {
-  return `SELECT ?class ?classLabel ?instance ?instanceLabel WHERE {
-    ?class wdt:P279* wd:${root} .
-    ?instance wdt:P31 ?class .
-    SERVICE wikibase:label {
-        bd:serviceParam wikibase:language "${language}" .
-    }
-}`
-}
-
-// Ask query.wikidata.org via SPARQL and simplify results
-function sparqlQuery(sparql) {
-  var url = wdk.sparqlQuery(sparql)
-  return request(url).then(wdk.simplifySparqlResults)
-}
-
-// Build graph data from root item and SPARQL results
-function buildGraph(root, results) {
-  var graph = buildTree(results[0])
-
-  graph.root = root
-
-  if (results[1]) {
-    var instances = graph.instances = {}
-    var items = graph.items
-    results[1].forEach(function(row) {
-      var id = row.class.value
-      if (!instances[id]) instances[id] = []
-      if (!items[row.instance.value]) {
-        items[row.instance.value] = row.instance
-      }
-      instances[id].push(row.instance.value)
-    })
-
-    for(var id in instances) {
-      instances[id] = instances[id].sort(function(x,y) {
-        return items[x].value.substr(1) - items[y].value.substr(1)
-      })
-    }
-  }
-  return graph;
-}
-
-// build tree structure from results
-function buildTree(results) {
-  var items    = {}
-  var narrower = {}
-  var broader  = {}
-
-  results.forEach(function(row) {
-    var qid       = row.item.value
-    var broaderId = row.broader
-
-    if (broaderId) {
-      if (narrower[broaderId]) {
-        narrower[broaderId].push(qid)
-      } else {
-        narrower[broaderId] = [qid]
-      }
-    }
-
-    if (!items[qid]) {
-      item = {
-        value:     qid,
-        label:     row.item.label || "",
-        parents:   row.parents,
-        instances: row.instances,
-        sites:     row.sites,
-        broader:   []
-      }
-      if (item.label == row.item.value) item.label = ""
-      items[qid] = item
-    }
-  })
-
-  for (var id in narrower) {
-    // sort child nodes by Wikidata id
-    narrower[id] = narrower[id].sort(function(x,y) {
-      return x.substr(1) - y.substr(1)
-    })
-    // add reverse
-    narrower[id].forEach(function(b) {
-      if (!broader[b]) broader[b] = []
-      broader[b].push(id)
-    })
-  }
-
-  for (var id in items) {
-    var item = items[id]
-    item.otherparents = item.parents
-    if (broader[id]) {
-      broader[id].forEach(function(node) {
-        if (items[node]) item.otherparents--
-      })
-    }
-  }
-
-  return { items: items, narrower: narrower, broader: broader }
 }
 
 program
@@ -185,19 +25,20 @@ program
   .option('-r, --reverse', 'get superclasses instead of subclasses')
   .option('-o, --output [file]', 'write result to a file')
   .description('extract taxonomies from Wikidata')
-  .action(function(id, env) {
+  .action(function(wid, env) {
     if (!env.colors) {
       chalk = new chalk.constructor({enabled: false});
     }
-    id = normalizeId(id)
+    var id = wdt.normalizeId(wid)
+    if (id === undefined) {
+      error(1,"invalid id: %s", wid)
+    }
     if (id.substr(0,1) == 'P') {
       env.properties = true
     }
 
     var out = process.stdout
     if (env.output) {
-      out = require('fs').createWriteStream(env.output)
-      out.on('error', function(err) { error(2,err) })
       var ext = env.output.split('.').pop()
       if (!env.format && (ext == 'csv' || ext == 'json')) {
         env.format = ext
@@ -215,26 +56,26 @@ program
       error(1,"option instances and reverse cannot be specified together");
     }
 
-    var queries = [ mainSparqlQuery(id, env) ]
-    if (env.instances && !env.properties) {
-      queries.push( instancesSparqlQuery(id, env.language) )
+    var out = function(file) {
+      if (!file) return process.stdout
+      var stream = require('fs').createWriteStream(file)
+      stream.on('error', function(err) { error(2,err) })
+      return stream
     }
 
+    var queries = wdt.sparql.queries(id, env)
+       
     if (env.sparql) {
-      out.write(queries.join("\n"))
+      out(env.output).write(queries.join("\n"))
     } else {
-      Promise.all( queries.map(sparqlQuery) )
-        .then(function(results) {
-          graph = buildGraph(id, results)
-          if (env.reverse) {
-            var tmp = graph.narrower
-            graph.narrower = graph.broader
-            graph.broader = tmp
-          }
-          out.write(wdt.serialize(graph, format))
-        })
-        .catch(function(err) {
+      Promise.all( queries.map(wdt.query) )
+        .catch((err) => {
           error(2,"SPARQL request failed!")
+        })
+        // TODO: check if only one result and test for existence/type
+        .then((results) => {
+          graph = wdt.build(id, results, env.reverse)
+          out(env.output).write(wdt.serialize(graph, format))
         })
     }
   })
